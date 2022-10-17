f03eee9819c745ac5250b6b56c086509f40a86a4
Renzo Quispe Dev <renzo.quispe.c@uni.pe>
2022-10-17 05:12:10 +0000

    Update ProjectsPortfolio.scss and ProjectsPortfolio.tsx

--- src/ProjectsPortfolio/ProjectsPortfolio.tsx
+++ src/ProjectsPortfolio/ProjectsPortfolio.tsx
@@ -8,6 +8,168 @@ const ProjectsPortfolio = ({
   altImage,
   withText,
 }: ProjectsPortfolioProps) => {
+  const ProjectItemLeft = (props: any) => (
+    <div className={"RQ-my-5"} style={{ margin: "50px 0 90px 0" }}>
+      <Row className={"About RQ-d-flex project-content"} noGutter>
+        <Col xs={18} other={11} className={"project-img"}>
+          <img src={props.content.img.source} alt={"ProjectImg"} />
+        </Col>
+        <Col xs={18} other={7} className={"project-desc"}>
+          <p className={"RQ-theme RQ-f-14 RQ-text-right"}>
+            {props.content.description.type}
+          </p>
+          <h1 className={"RQ-f-28 RQ-text-right"}>
+            {props.content.description.title}
+          </h1>
+          <div className="project-summary">
+            <p className={"RQ-f-17 RQ-white RQ-text-center"}>
+              {props.content.description.info}
+            </p>
+          </div>
+          <p className={"RQ-my-4 RQ-text-right"}>
+            {props.content.description.stack}
+          </p>
+          <div className="project-links">
+            <a
+              href={props.content.description.github}
+              aria-label="GitHub Link"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="project-icon RQ-mx-2"
+                data-darkreader-inline-stroke=""
+              >
+                <title>GitHub</title>
+                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+              </svg>
+            </a>
+            <a
+              href={props.content.description.url}
+              aria-label="External Link"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="project-icon RQ-mx-2"
+                data-darkreader-inline-stroke=""
+              >
+                <title>External Link</title>
+                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line>
+              </svg>
+            </a>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+
+  const ProjectItemRight = (props: {
+    content: {
+      img: {
+        source: string;
+      };
+      description: {
+        type: string;
+        title: string;
+        info: string;
+        stack: string;
+        github: string;
+        url: string;
+      };
+    };
+  }) => (
+    <div className={"RQ-my-5"} style={{ margin: "50px 0 90px 0" }}>
+      <Row className={"About RQ-d-flex project-content-opposite"} noGutter>
+        <Col xs={18} other={7} className={"project-desc"}>
+          <p className={"RQ-theme RQ-f-14 RQ-text-left"}>
+            {props.content.description.type}
+          </p>
+          <h1 className={"RQ-f-28 RQ-text-left"}>
+            {props.content.description.title}
+          </h1>
+          <div className="project-summary">
+            <p className={"RQ-f-17 RQ-white RQ-text-center"}>
+              {props.content.description.info}
+            </p>
+          </div>
+          <p className={"RQ-my-4 RQ-text-left"}>
+            {props.content.description.stack}
+          </p>
+          <div className="project-links">
+            <a
+              href={props.content.description.github}
+              aria-label="GitHub Link"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="project-icon RQ-mx-2"
+                data-darkreader-inline-stroke=""
+              >
+                <title>GitHub</title>
+                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+              </svg>
+            </a>
+            <a
+              href={props.content.description.url}
+              aria-label="External Link"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="project-icon RQ-mx-2"
+                data-darkreader-inline-stroke=""
+              >
+                <title>External Link</title>
+                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line>
+              </svg>
+            </a>
+          </div>
+        </Col>
+        <Col xs={18} other={11} className={"project-img"}>
+          <img src={props.content.img.source} alt={"ProjectImg"} />
+        </Col>
+      </Row>
+    </div>
+  );
+
   return (
     <div data-testid="ProjectsPortfolio" className={"ProjectsPortfolio"}>
       <div className={"RQ-d-flex RQ-align-center RQ-mb-4 RQ-ml-3 RQ-w-50"}>
@@ -18,83 +180,50 @@ const ProjectsPortfolio = ({
       <p className={"RQ-ml-6 RQ-mb-5 RQ-f-16"}>
         Each project is unique. Here are some of my works.
       </p>
-      <div className={"RQ-mt-2"}>
-        <Row className={"About RQ-d-flex project-content"} noGutter>
-          <Col xs={18} other={11} className={"project-img"}>
-            <img
-              src={
-                "https://definicion.de/wp-content/uploads/2009/12/paisaje-1.jpg"
-              }
-              alt={altImage}
-            />
-          </Col>
-          <Col xs={18} other={7} className={"project-desc"}>
-            <p className={"RQ-theme RQ-f-14 RQ-text-right"}>Featured Project</p>
-            <h1 className={"RQ-f-28 RQ-text-right"}>
-              Build a Spotify Connected App
-            </h1>
-            <div className="project-summary">
-              <p className={"RQ-f-17 RQ-white RQ-text-center"}>
-                A <strong>minimal</strong>, dark blue theme for VS Code, Sublime
-                Text, Atom, iTerm, and more. Available on Visual Studio
-                Marketplace, Package Control,{" "}
-                <a href="#">Atom Package Manager</a>, and npm.
-              </p>
-            </div>
-            <p className={"RQ-my-4 RQ-text-right"}>
-              React Styled Components, Express, Spotify API, Heroku
-            </p>
-            <div className="project-links">
-              <a
-                href="https://github.com/bchiang7/halcyon-site"
-                aria-label="GitHub Link"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="project-icon RQ-mx-2"
-                  data-darkreader-inline-stroke=""
-                >
-                  <title>GitHub</title>
-                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                </svg>
-              </a>
-              <a
-                href="https://halcyon-theme.netlify.com/"
-                aria-label="External Link"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="project-icon RQ-mx-2"
-                  data-darkreader-inline-stroke=""
-                >
-                  <title>External Link</title>
-                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                  <polyline points="15 3 21 3 21 9"></polyline>
-                  <line x1="10" y1="14" x2="21" y2="3"></line>
-                </svg>
-              </a>
-            </div>
-          </Col>
-        </Row>
-      </div>
+      <ProjectItemLeft
+        content={{
+          img: {
+            source:
+              "https://definicion.de/wp-content/uploads/2009/12/paisaje-1.jpg",
+          },
+          description: {
+            type: "Featured Project",
+            title: "Build a Spotify Connected App",
+            info: [
+              "A ",
+              <strong>minimal</strong>,
+              ", dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, ",
+              <a href="#">Atom Package Manager</a>,
+              ", and npm.",
+            ],
+            stack: "React Styled Components, Express, Spotify API, Heroku",
+            github: "https://github.com/bchiang7/halcyon-site",
+            url: "https://halcyon-theme.netlify.com/",
+          },
+        }}
+      />
+      <ProjectItemRight
+        content={{
+          img: {
+            source:
+              "https://definicion.de/wp-content/uploads/2009/12/paisaje-1.jpg",
+          },
+          description: {
+            type: "Featured Project",
+            title: "Build a Spotify Connected App",
+            info: [
+              "A ",
+              <strong>minimal</strong>,
+              ", dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, ",
+              <a href="#">Atom Package Manager</a>,
+              ", and npm.",
+            ],
+            stack: "React Styled Components, Express, Spotify API, Heroku",
+            github: "https://github.com/bchiang7/halcyon-site",
+            url: "https://halcyon-theme.netlify.com/",
+          },
+        }}
+      />
     </div>
   );
 };
