3b93ca558d0fd7aa884af2afde296ed36dfd269a
renzo <renzo.quispe.c@uni.pe>
2022-11-25 21:58:09 -0500

    fixing svg build

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
